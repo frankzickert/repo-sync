5cc5cc03f9b2f89c6f30c2e7454260eb60d2c327
Frank Zickert <webmaster@clued-up.de>
2020-07-06 09:54:04 +0200

    fixed folders

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,11 +7,7 @@ GITHUB_USERNAME=$1
 STARTER_NAME="${2:-name}"
 BASE=$(pwd)
 CLONE_DIR="__${STARTER_NAME}__clone__"
-
-git clone --depth 1 https://$API_TOKEN_GITHUB@github.com/$GITHUB_USERNAME/$STARTER_NAME.git $CLONE_DIR &> /dev/null
-cd $CLONE_DIR
-find . | grep -v ".git" | grep -v "^\.*$" | xargs rm -rf # delete all files (to handle deletions in monorepo)
-  
+REPO = https://$API_TOKEN_GITHUB@github.com/$GITHUB_USERNAME/$STARTER_NAME.git
 
 git config --global user.email "fzickert@googlemail.com"
 git config --global user.name "$GITHUB_USERNAME"
@@ -19,7 +15,14 @@ git config --global user.name "$GITHUB_USERNAME"
 echo "Cloning folders and pushing to $GITHUB_USERNAME"
 echo "Using $STARTER_NAME as the repo name"
 
+
+echo "clone $CLONE_DIR $REPO"
+git clone --depth 1 $REPO $CLONE_DIR &> /dev/null
+cd $CLONE_DIR
+find . | grep -v ".git" | grep -v "^\.*$" | xargs rm -rf # delete all files (to handle deletions in monorepo)
+
 cd $BASE
+
 
 # sync to read-only clones
 for folder in *; do
